f1475f23e537ad7df8d9d3fd2d06f4702f829e8a
vovastelmashchuk <vovochkastelmashchuk@gmail.com>
2025-05-22 22:04:45 +0300

    add fallback to audio sync api

--- server/plugins/audioSync.js
+++ server/plugins/audioSync.js
@@ -22,7 +22,7 @@ export default defineNitroPlugin(async (_) => {
         if (episode) {
             console.info(
                 'Syncing audio for episode',
-                episode._id,
+                episode.slug,
                 new Date().toISOString()
             )
             await syncAudio(db, episode)
@@ -34,31 +34,16 @@ export default defineNitroPlugin(async (_) => {
 
 async function syncAudio(db, episode) {
     try {
-        const body = {
-            url: `https://www.youtube.com/watch?v=${episode.youtubeVideoId}`
-        }
-        const youtubeToMp3Response = await $fetch(
-            `https://youtube-to-mp335.p.rapidapi.com/api/converttomp3`,
-            {
-                method: 'POST',
-                body: body,
-                headers: {
-                    'x-rapidapi-host': 'youtube-to-mp335.p.rapidapi.com',
-                    'x-rapidapi-key': rapidApiKey
-                }
-            }
-        )
+        const youtubeVideoInfo = await getYoutubeInfo(episode.youtubeVideoId, episode.slug, db)
+        const audioDownloadUrl = youtubeVideoInfo.audioUrl
 
-        console.info('youtube to mp3 response', youtubeToMp3Response)
-
-        const audioDownloadUrl = youtubeToMp3Response.url
-        const duration = youtubeToMp3Response.duration
+        const duration = youtubeVideoInfo.duration
         const audioFileName = `${episode.slug}.mp3`
 
         await downloadFile(BUCKET.audio, audioFileName, audioDownloadUrl)
 
         await db.collection('episodes').updateOne(
-            { _id: episode._id },
+            { slug: episode.slug },
             {
                 $set: {
                     audioSyncStatus: 'done',
@@ -69,10 +54,10 @@ async function syncAudio(db, episode) {
             }
         )
     } catch (error) {
-        console.error('Failed to sync audio for episode', episode._id, error)
+        console.error('Failed to sync audio for episode', episode.slug, error)
 
         await db.collection('episodes').updateOne(
-            { _id: episode._id },
+            { slug: episode.slug },
             {
                 $set: {
                     audioSyncStatus: 'error',
@@ -81,4 +66,112 @@ async function syncAudio(db, episode) {
             }
         )
     }
+}
+
+async function getYoutubeInfo(youtubeVideoId, episodeSlug, db) {
+    try {
+        return await getYoutubeLinkByYoutubeToMp3(youtubeVideoId, episodeSlug, db)
+    } catch (error) {
+        await addEpisodeLog(db, episodeSlug, 'error', `Failed to get youtube download link by youtube-to-mp335`);
+        try {
+            return await getYoutubeLinkByYoutubeMp36(youtubeVideoId, episodeSlug, db)
+        } catch (error) {
+            await addEpisodeLog(db, episodeSlug, 'error', `Failed to get youtube download link by youtube-mp36`);
+            throw error
+        }
+    }
+}
+
+//youtube-to-mp335
+async function getYoutubeLinkByYoutubeToMp3(youtubeVideoId, episodeSlug, db) {
+    await addEpisodeLog(db, episodeSlug, 'info', `Getting youtube link by youtube-to-mp335`);
+    const body = {
+        url: `https://www.youtube.com/watch?v=${youtubeVideoId}`
+    }
+    const youtubeToMp3Response = await $fetch(
+        `https://youtube-to-mp335.p.rapidapi.com/api/converttomp3`,
+        {
+            method: 'POST',
+            body: body,
+            headers: {
+                'x-rapidapi-host': 'youtube-to-mp335.p.rapidapi.com',
+                'x-rapidapi-key': rapidApiKey
+            }
+        }
+    )
+
+    console.info('youtube to mp3 response', youtubeToMp3Response)
+
+    await addEpisodeLog(db, episodeSlug, 'info', `Youtube to mp3 response: ${JSON.stringify(youtubeToMp3Response)}`);
+
+    return {
+        audioUrl: youtubeToMp3Response.url,
+        duration: youtubeToMp3Response.duration
+    }
+}
+
+//youtube-mp36
+async function getYoutubeLinkByYoutubeMp36(youtubeVideoId, episodeSlug, db) {
+    await addEpisodeLog(db, episodeSlug, 'info', `Getting youtube link by youtube-mp36`);
+    const url = `https://youtube-mp36.p.rapidapi.com/dl?id=${youtubeVideoId}`;
+    const headers = {
+        'x-rapidapi-key': rapidApiKey,
+        'x-rapidapi-host': 'youtube-mp36.p.rapidapi.com'
+    };
+
+    const maxAttempts = 10;
+    let attempt = 0;
+
+    while (attempt < maxAttempts) {
+        attempt++;
+        // Log the attempt
+        await addEpisodeLog(db, episodeSlug, 'info', `Attempt ${attempt}: Trying to get audio URL from youtube-mp36`);
+
+        try {
+            const response = await $fetch(url, {
+                method: 'GET',
+                headers
+            });
+
+            console.info('youtube-mp36 response', response);
+
+            // Log the response
+            await addEpisodeLog(db, episodeSlug, 'info', `Attempt ${attempt}: Received response: ${JSON.stringify(response)}`);
+
+            if (response.status === 'ok' && response.link) {
+                return {
+                    audioUrl: response.link,
+                    duration: response.duration
+                }
+            }
+
+            // Wait for 1 minute before retrying
+            await new Promise((resolve) => setTimeout(resolve, 60 * 1000));
+        } catch (error) {
+            console.error('Failed to get youtube-mp36 link', error);
+            // Log the error
+            await addEpisodeLog(db, episodeSlug, 'error', `Attempt ${attempt}: Error occurred: ${error.message}`);
+            await new Promise((resolve) => setTimeout(resolve, 60 * 1000));
+        }
+    }
+
+    // Log the final failure
+    await addEpisodeLog(db, episodeSlug, 'error', `Failed to get youtube-mp36 link after ${maxAttempts} attempts`);
+
+    throw new Error('Failed to get youtube-mp36 link after 10 attempts');
+}
+
+async function addEpisodeLog(db, episodeSlug, type, message) {
+    await db.collection('episodes').updateOne(
+        { slug: episodeSlug },
+        {
+            $push: {
+                logs: {
+                    timestamp: new Date(),
+                    type,
+                    message
+                }
+            }
+        }
+    );
 }
